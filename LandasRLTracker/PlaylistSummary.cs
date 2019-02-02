b5bc19d60915a71fd9cc844167e4e48667f21620
BlancoLanda <martinblancolanda7@gmail.com>
2019-02-02 16:12:17 +0100

    v2.0: Bye bye console... Welcome GUI!

---
+++ LandasRLTracker/PlaylistSummary.cs
@@ -0,0 +1,13 @@
+﻿namespace LandasRLTracker
+{
+public class PlaylistSummary
+    {
+        public string Playlist { get; set; }
+        public int Games { get; set; }
+        public string W { get; set; }
+        public int L { get; set; }
+        public string MMR { get; set; }
+        public string Initial_Rank { get; set; }
+        public string Current_Rank { get; set; }
+    }
+}
