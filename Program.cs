a240124d2384fd5d3d9d4a4bfa31fe2b6e2b6e36
BlancoLanda <martinblancolanda7@gmail.com>
2019-02-03 11:32:26 +0100

    Restrict application to just one instance

--- Program.cs
+++ Program.cs
@@ -1,19 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace LandasRLTracker
 {
     static class Program
     {
+        private static Mutex mutex = null;
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
+
         static void Main()
         {
+            const string appName = @"Landa's RL Tracker";
+
+            mutex = new Mutex(true, appName, out bool createdNew);
+
+            if (!createdNew)
+            {
+                //app is already running! Exiting the application  
+                return;
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
